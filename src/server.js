ccb2c44551191b720a5114a7d54a5e31fcf015ca
Zakhar-T <zakhar.null@gmail.com>
2025-07-05 13:40:52 +0300

    added all the connection files

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import pino from 'pino';
+import pino from 'pino-http';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
-dotenv.config();
+import { getEnvVar } from './utils/getEnvVar.js';
 
-const PORT = Number(process.env.PORT);
+const PORT = getEnvVar('PORT');
 
 export const setupServer = () => {
   const app = express();
@@ -19,11 +18,28 @@ export const setupServer = () => {
   );
   app.use(cors());
 
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.get('/students', (req, res) => {
+    res.json({
+      message: 'List of students',
+    });
+  });
+
+  app.use('', (req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
   app.listen(PORT, (error) => {
     if (error) {
       throw new Error(error);
     }
-
     console.log(`Server is running on port ${PORT}`);
   });
 };
