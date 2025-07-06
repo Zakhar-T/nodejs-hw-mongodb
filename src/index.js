052dc7762eba8b4e181132307d6a8352e420cdcb
Zakhar-T <zakhar.null@gmail.com>
2025-07-06 22:28:42 +0300

    edit files names

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { initMongoDB } from './db/initMongoDB.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
 const startServer = async () => {
-  await initMongoDB();
+  await initMongoConnection();
   setupServer();
 };
 
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,11 @@
 import { ContactsCollection } from '../db/models/contact.js';
 
 export const getAllContacts = async () => {
-  const contacts = ContactsCollection.find();
+  const contacts = await ContactsCollection.find();
   return contacts;
 };
 
 export const getContactById = async (id) => {
-  const contact = ContactsCollection.findById(id);
+  const contact = await ContactsCollection.findById(id);
   return contact;
 };
